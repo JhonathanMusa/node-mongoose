cf1f0338149556f56c0520b41dfcd4e5a314db52
jhonathanmusa <jonathansm891@gmail.com>
2020-10-27 13:20:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+
+const PORT = 8080;
+const url = "mongodb://localhost/moviesDB";
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// bodyparser
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
