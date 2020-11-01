96d7c2866ef07e153d43243effc6839d61e6308d
jhonathanmusa <jonathansm891@gmail.com>
2020-10-31 21:52:42 -0500

    change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const { Schema } = mongoose.Schema();
 
 const PORT = 8080;
 const url = "mongodb://localhost/moviesDB";
@@ -17,7 +18,16 @@ mongoose.connect(url, {
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-app.use(require("./src/routes/index"));
+// Schema
+const movieSchema = Schema({
+  title: String,
+  year: Number,
+  genre: String,
+});
+
+const movie = mongoose.model("movie", movieSchema);
+
+// app.use(require("./src/routes/index"));
 
 app.get("/", (req, res) => {
   res.send("Welcome to index page");
--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const { getMovies } = require("../controllers/index.controller");
-const routes = (app) => {
-  app.route("/movies").get(getMovies);
-};
-
-module.exports = routes;
